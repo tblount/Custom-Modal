0a487c6df2b6dbeb32b128c9d1ddae5486734975
Terrance CyberpunkN0mad <tblountcs@gmail.com>
2024-11-08 07:09:54 -0600

    Updated App,js to include Header and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import './styles.css';
