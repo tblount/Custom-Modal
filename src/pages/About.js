259a5e3804b562d615923e36c93877393aeea577
Terrance <tblountcs@gmail.com>
2024-11-08 05:12:41 -0600

    Added an About page

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+// src/pages/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is a simple app using a modal to recall old chats.</p>
+    </div>
+  );
+};
+
+export default About;
