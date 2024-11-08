c911190e522e6ffe22c656c2576aae43f93d4291
Terrance <tblountcs@gmail.com>
2024-11-08 05:53:00 -0600

    Updated Header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+// src/components/Header.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
