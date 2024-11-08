a9cf255d38dba3de718379eb5c1a3c6277c38ad5
Terrance <tblountcs@gmail.com>
2024-11-08 05:26:39 -0600

    Added routes for pages and App component

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import './styles.css';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
